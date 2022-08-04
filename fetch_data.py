db2602e84ae4c4815e42cb450b5185d4a3123d20
trancenoid <shivansh.bhr@gmail.com>
2022-08-04 16:35:56 +0200

    "reorganization for dvc"

--- fetch_data.py
+++ fetch_data.py
@@ -2,29 +2,34 @@ from pipeline.core import *
 from pipeline.settings import Settings
 import sys
 import yaml
+import json 
 
 params = yaml.safe_load(open("params.yaml"))["fetch_data"]
 
-# load config files
-settings = Settings(
-    conn_json_path="pipeline/connection.json",
-    train_data_json_path="pipeline/training_data_config.json", 
-    filter_json_path="pipeline/data_filters.json", 
-    train_json_path="pipeline/train_params.json", 
-    test_json_path="pipeline/test_params.json")
+with open("pipeline/training_data_config.json", 'rb') as f:
+    train_data_config = json.load(f)
+
+        
+with open("pipeline/connection.json", 'rb') as f:
+    conn_config = json.load(f)
+
+
+with open("pipeline/data_filters.json", 'rb') as f:
+    filters_config = json.load(f)
+
 
 # connect to DB 
-connection = connect_to_db(settings.conn_config['connection_string'][params['server_type']])
+connection = connect_to_db(conn_config['connection_string'][params['server_type']])
 
 # fetch data with supplied filters
 data = fetch_data(
     connection, 
-    settings.filters_config['product_ids'],
-    settings.filters_config['channel_ids']
+    filters_config['product_ids'],
+    filters_config['channel_ids']
     )
 
 # preprocess data and compute target (weight) columns
 data = process_df(data)
 
 # create moving window splits for evaluation
-create_sliding_window_splits(data,settings.train_data_config['moving_window_size'])
+create_sliding_window_splits(data,train_data_config['moving_window_size'])
